13ed5dce94d43c9503ad95f36b26a7a957997155
fcoury <fcoury@e6aea695-393b-0410-bcd1-a96c4ba3c4be>
2007-10-08 01:42:14 +0000

    Test Suite for running all tests at once
    git-svn-id: https://jfilehelpers.svn.sourceforge.net/svnroot/jfilehelpers@8 e6aea695-393b-0410-bcd1-a96c4ba3c4be

---
+++ Tests/src/org/coury/jfilehelpers/tests/AllTests.java
@@ -0,0 +1,44 @@
+/*
+ * AllTests.java
+ *
+ * Copyright (C) 2007 Felipe Gonçalves Coury <felipe.coury@gmail.com>
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package org.coury.jfilehelpers.tests;
+
+import org.coury.jfilehelpers.tests.converters.DateFormatTest;
+import org.coury.jfilehelpers.tests.converters.DecimalNumbersTest;
+import org.coury.jfilehelpers.tests.converters.EnumConverterTest;
+import org.coury.jfilehelpers.tests.masterdetail.MasterDetailTest;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class AllTests {
+
+	public static Test suite() {
+		TestSuite suite = new TestSuite(
+				"Test for org.coury.jfilehelpers.tests.converters");
+		//$JUnit-BEGIN$
+		suite.addTestSuite(EnumConverterTest.class);
+		suite.addTestSuite(DateFormatTest.class);
+		suite.addTestSuite(DecimalNumbersTest.class);
+		suite.addTestSuite(MasterDetailTest.class);
+		//$JUnit-END$
+		return suite;
+	}
+
+}
